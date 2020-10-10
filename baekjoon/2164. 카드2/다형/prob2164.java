2d7d92545623d580ea7d25551651044221d87c31
Yoo Dahyoung <55521320+dhyoo99@users.noreply.github.com>
2020-10-10 15:18:23 +0900

    Create prob2164.java

---
+++ baekjoon/2164. 카드2/다형/prob2164.java
@@ -0,0 +1,25 @@
+package chap04;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class prob2164 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int N = stdIn.nextInt();
+		LinkedList<Integer> card = new LinkedList<>();//큐 생성, 최대 N개를 인큐할 수 있는 큐
+		
+		for (int i=0; i<N; i++) {
+			card.add(i+1); // 1부터 N까지 순서대로 인큐  
+		}
+		
+		do {
+			card.remove();
+			int x = card.remove();
+			card.add(x); // 두 번 deque하고 두 번째로 디큐한 값을 다시 인큐  
+		} while (card.size() > 1);
+		
+		int answer = card.poll();
+		System.out.println(answer);
+	}
+}
