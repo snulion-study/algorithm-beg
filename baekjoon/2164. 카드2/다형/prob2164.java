5a363ae424ccd678bbd43adb57af7ac300c144d2
Yoo Dahyoung <55521320+dhyoo99@users.noreply.github.com>
2020-10-12 00:47:19 +0900

    Update prob2164.java
    런타임에러 수정(do~while문을 while문으로 수정)

--- baekjoon/2164. 카드2/다형/prob2164.java
+++ baekjoon/2164. 카드2/다형/prob2164.java
@@ -13,11 +13,11 @@ public class prob2164 {
 			card.add(i+1); // 1부터 N까지 순서대로 인큐  
 		}
 		
-		do {
+		while(card.size()>1){
 			card.remove();
 			int x = card.remove();
 			card.add(x); // 두 번 deque하고 두 번째로 디큐한 값을 다시 인큐  
-		} while (card.size() > 1);
+		}
 		
 		int answer = card.poll();
 		System.out.println(answer);
